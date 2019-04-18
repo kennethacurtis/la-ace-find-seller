a409ef2759fbcad2ce98c181682c527a16208cf8
kacurtis <kacurtis1616@gmail.com>
2019-04-17 21:58:09 -0400

    add sudo

--- products/build/build.sh
+++ products/build/build.sh
@@ -5,4 +5,4 @@
 
 # If you change the tag of la-ace-products:0.1 then edit the build.sh for this service too. 
 # Just do a find and replace for "la-ace-products:0.1"
-docker build -t la-ace-products:0.1 -f "$(dirname "$0")/Dockerfile" "$(dirname "$0")/../"
+sudo docker build -t la-ace-products:0.1 -f "$(dirname "$0")/Dockerfile" "$(dirname "$0")/../"
