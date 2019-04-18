7f73c4d6c8009d7450fd22c2a67d3113816e6125
kacurtis <kacurtis1616@gmail.com>
2019-04-17 20:06:01 -0400

    update cluster version

--- products/cloud/setup.sh
+++ products/cloud/setup.sh
@@ -62,7 +62,7 @@ gcloud beta container clusters create $PRODUCT_CLUSTER_NAME \
     --project $PROJECT_NAME \
     --zone $PROJECT_ZONE \
     --no-enable-basic-auth \
-    --cluster-version "1.9.7-gke.N" \
+    --cluster-version "1.11.7-gke.12" \
     --machine-type "n1-standard-1" \
     --image-type "COS" \
     --disk-type "pd-standard" \
